f24b53fcb4be3544f8be283c0a6b7a080bb9a102
Лука Руслан <lukasus22mail.ru@gmail.com>
2021-03-06 18:05:27 +0300

    Оконченное ядро таблицы
    !!!Требуется доработка!!!

---
+++ dbo.characteristics_table.data.sql
@@ -0,0 +1,5 @@
+﻿SET IDENTITY_INSERT [dbo].[characteristics_table] ON
+INSERT INTO [dbo].[characteristics_table] ([characteristics_id], [heigh], [width], [depth]) VALUES (1, N'10', N'5', N'5')
+INSERT INTO [dbo].[characteristics_table] ([characteristics_id], [heigh], [width], [depth]) VALUES (2, N'5', N'2', NULL)
+INSERT INTO [dbo].[characteristics_table] ([characteristics_id], [heigh], [width], [depth]) VALUES (3, N'2', N'1', N'1')
+SET IDENTITY_INSERT [dbo].[characteristics_table] OFF
---
+++ dbo.photo_table.data.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[photo_table] ON
+INSERT INTO [dbo].[photo_table] ([photo_id], [photo], [type_id], [characteristics_id]) VALUES (3, (SELECT * FROM OPENROWSET(BULK N'E:\Diplom\DefectsDMS\bin\Debug\Photo\3.jpg', SINGLE_BLOB) AS image), 3, 3)
+SET IDENTITY_INSERT [dbo].[photo_table] OFF
