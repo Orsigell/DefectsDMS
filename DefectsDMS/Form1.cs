450dbb8e54ea9c81a020377caf9a2de58c829b04
Лука Руслан <lukasus22mail.ru@gmail.com>
2021-03-14 18:53:27 +0300

    Добавлено открытие соединения с БД
    Добавлено получение данных из БД
    
    Обработано закрытие программы

--- DefectsDMS/Form1.cs
+++ DefectsDMS/Form1.cs
@@ -7,15 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace DefectsDMS
 {
     public partial class Form1 : Form
     {
+        SqlConnection sqlCon = null;
+        SqlDataAdapter sqlAdapter = null;
+        DataTable table = new DataTable();
         public Form1()
         {
             InitializeComponent();
         }
-        private void выходToolStripMenuItem_Click(object sender, EventArgs e) => Application.Exit();
+        private void GetDataFromDB(string command)
+        {
+            sqlAdapter = new SqlDataAdapter(command, sqlCon);
+            table.Clear();
+            sqlAdapter.Fill(table);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlCon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database.mdf;Integrated Security=True");
+            sqlCon.Open();
+        }
+        private void ProgramClose() { sqlCon.Close(); Application.Exit(); }
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e) => ProgramClose();    
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e) => ProgramClose();        
     }
 }
